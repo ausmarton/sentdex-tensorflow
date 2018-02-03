a0a8c7970ebf546e11e643e711c663743a51ba46
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2018-02-03 20:19:37 +0000

    Adding the first two samples from the series with changes only to fix warnings/errors

---
+++ 01-tfbasics.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import tensorflow as tf
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+# tf.mul has been replaced with tf.multiply according to:
+# https://stackoverflow.com/questions/42217059/tensorflowattributeerror-module-object-has-no-attribute-mul
+result = tf.multiply(x1,x2)
+print(result)
+
+with tf.Session() as sess:
+    output = sess.run(result)
+    print(output)
+    
+print(output)
+
