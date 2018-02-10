4c0e16d9f0c22f8816453f5ddda138d6af09c2e3
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2018-02-10 22:31:13 +0000

    Forecast with regression

--- 01-ml-regression.py
+++ 01-ml-regression.py
@@ -1,10 +1,14 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 # preprocessing => scaling data
 # cross_validation => to create training and testing samples
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -20,12 +24,15 @@ df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -35,5 +42,29 @@ clf = LinearRegression()
 # clf = svm.SVR()
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
+
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+	next_date = datetime.datetime.fromtimestamp(next_unix)
+	next_unix += one_day
+	df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+print(df.tail)
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
 
 print(accuracy)
